919451413ae7f8deab8749ef7527eac60765cd53
00010389 <wiut00010389@gmail.com>
2022-11-03 23:54:31 +0500

    Make products view a default view

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace decay_gaming_shop
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Products", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
